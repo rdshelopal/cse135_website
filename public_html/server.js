abea12e5a9c9e0addc06fcf47840ff865a30e5a7
rdshelopal <rdshelopal@gmail.com>
2025-09-01 20:35:50 -0700

    sql fix

--- public_html/server.js
+++ public_html/server.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
   host: '127.0.0.1',
   user: 'root',
-  password: '',
+  password: '*Pizzaballs56!',
   database: 'cse135'
 });
 
